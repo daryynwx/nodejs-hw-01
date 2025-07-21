5edf75f4b2c33939f58a15411e294a9765c5d093
Darina Boinik <darinaboinik@gmail.com>
2025-07-21 20:21:09 +0300

    add all

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,11 @@
-const generateContacts = async (number) => {};
+import { createFakeContact } from "../utils/createFakeContact.js";
+import { readContacts } from "../utils/readContacts.js";
+import { writeContacts } from "../utils/writeContacts.js";
 
-generateContacts(5);
+const generateContacts = async (count = 5) => {
+    const oldContacts = await readContacts();
+    const newContacts = Array.from({ length: count }, createFakeContact);
+    await writeContacts([...oldContacts, ...newContacts]);
+};
+
+generateContacts();
--- src/scripts/removeAllContacts.js
+++ src/scripts/removeAllContacts.js
@@ -1,3 +1,7 @@
-export const removeAllContacts = async () => {};
+import { writeContacts } from "../utils/writeContacts.js";
+
+const removeAllContacts = async () => {
+    await writeContacts([]);
+};
 
 removeAllContacts();
